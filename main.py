dec0d8f750f8405b8f42ad99d6a24fe61c5fd945
dhara <dharanimoorthy51@gmail.com>
2025-06-17 19:19:13 +0530

    basic

---
+++ main.py
@@ -0,0 +1,17 @@
+x=2;
+y=3;
+z=x+y;
+print(z);
+print(x//y);
+print(x/y);
+print(x**y);
+print(10%3);
+print(r'dharani\'s laptop\n ffff'),
+name=[2.5,'dharani',5]
+print (name,x)
+name.append('dd')
+print(name)
+tup=(21,23,45,66,78,89,90)
+print(tup[1])
+print(tup.count(21))
+print(tup.index(45))
